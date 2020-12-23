fd39732c58ea0ed08b5dea4f1b31d0f71e523253
郭勇 <>
2020-12-23 19:44:42 +0800

    Coordinator and TabLayout

---
+++ coordinatortablayout/src/main/java/cn/hugeterry/coordinatortablayout/listener/OnTabSelectedListener.java
@@ -0,0 +1,16 @@
+package cn.hugeterry.coordinatortablayout.listener;
+
+import com.google.android.material.tabs.TabLayout;
+
+/**
+ * @author hugeterry(http://hugeterry.cn)
+ */
+
+public interface OnTabSelectedListener {
+
+    public void onTabSelected(TabLayout.Tab tab);
+
+    public void onTabUnselected(TabLayout.Tab tab);
+
+    public void onTabReselected(TabLayout.Tab tab);
+}
---
+++ sample/src/main/java/cn/hugeterry/coordinatortablayoutdemo/MainActivity.java
@@ -0,0 +1,84 @@
+package cn.hugeterry.coordinatortablayoutdemo;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import cn.hugeterry.coordinatortablayout.CoordinatorTabLayout;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ */
+public class MainActivity extends AppCompatActivity {
+    private CoordinatorTabLayout mCoordinatorTabLayout;
+    private int[] mImageArray, mColorArray;
+    private ArrayList<Fragment> mFragments;
+    private final String[] mTitles = {"Android", "iOS", "Web", "Other"};
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFragments();
+        initViewPager();
+        mImageArray = new int[]{
+                R.mipmap.bg_android,
+                R.mipmap.bg_ios,
+                R.mipmap.bg_js,
+                R.mipmap.bg_other};
+        mColorArray = new int[]{
+                android.R.color.holo_blue_light,
+                android.R.color.holo_red_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_green_light};
+
+        mCoordinatorTabLayout = (CoordinatorTabLayout) findViewById(R.id.coordinatortablayout);
+        mCoordinatorTabLayout.setTranslucentStatusBar(this)
+                .setTitle("Demo")
+                .setBackEnable(true)
+                .setImageArray(mImageArray, mColorArray)
+                .setupWithViewPager(mViewPager);
+    }
+
+    private void initFragments() {
+        mFragments = new ArrayList<>();
+        for (String title : mTitles) {
+            mFragments.add(MainFragment.getInstance(title));
+        }
+    }
+
+    private void initViewPager() {
+        mViewPager = (ViewPager) findViewById(R.id.vp);
+        mViewPager.setOffscreenPageLimit(4);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), mFragments, mTitles));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                IntentUtils.openUrl(this, "https://github.com/hugeterry/CoordinatorTabLayout");
+                break;
+            case R.id.action_about_me:
+                IntentUtils.openUrl(this, "http://hugeterry.cn/about");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
